d2f026163a19893ce4f3ddffc7a094aa476f3ef0
John Davison <johncdavison@gmail.com>
2012-09-19 09:48:05 -0700

    more blog updates

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,11 +21,18 @@ class ArticlesController < ApplicationController
     end
     
     def edit
+        @article = Article.find(params[:id])
     end
     
     def update
+        @article = Article.find(params[:id])
+        @article.update_attributes(params[:article])
+        redirect_to articles_path
     end
     
     def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
     end   
 end

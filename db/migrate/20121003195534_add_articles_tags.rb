7cc08f801629153cee397c276a8a6a93d16a7c05
John Davison <johncdavison@gmail.com>
2012-10-04 12:06:09 -0700

    def f heroku

--- db/migrate/20121003195534_add_articles_tags.rb
+++ db/migrate/20121003195534_add_articles_tags.rb
@@ -1,5 +1,6 @@
 class AddArticlesTags < ActiveRecord::Migration
   def change
+    drop_table :articles_tags
     create_table :articles_tags, :id => false do |t|
       t.integer :article_id
       t.integer :tag_id

23604b5cb0af39afbf2c5433dfc60007481250b6
John Davison <johncdavison@gmail.com>
2012-12-04 09:48:57 -0800

    coffeescript removed

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Jcd::Application.routes.draw do
   
   #resources :disruptions
   root :to => 'articles#index'
+  match '/john_davison_is_really_a_great_roommate_and_this_is_why' => "articles#roommate"
+  
   
 
 end

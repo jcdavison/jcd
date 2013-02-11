0f2e8d261185d0e307ba969a8b0e4bb6cde4d3a7
John Davison <johncdavison@gmail.com>
2013-02-10 16:02:49 -0800

    general maint

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,7 @@ Jcd::Application.routes.draw do
   devise_for :users
   resources :abbs
   resources :articles
-  
   #resources :disruptions
   root :to => 'articles#index'
-  match '/john_davison_is_really_a_great_roommate_and_this_is_why' => "articles#roommate"
-  
-  
 
 end
